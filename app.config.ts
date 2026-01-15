ec48932c3f97dcd6e13c4867783b92269da13f69
jjzme2 <Jjzettler@gmail.com>
2026-01-15 15:07:21 -0600

    feat: Add admin launchpad tab and refactor application configuration into dedicated files, including `nuxt-gtag` dependency.

--- app.config.ts
+++ app.config.ts
@@ -1,52 +1,22 @@
-import { themes } from './config/themes'
-import { ui } from './config/ui'
+import {
+    themes
+    , ui
+    , socials
+    , quickLaunch
+    , meta
+    , security
+    , hoverMessages
+} from './config'
 
 export default defineAppConfig({
-    socials: {
-        // instagram: {
-        //     url: 'https://instagram.com/nicole',
-        //     icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png'
-        // },
-        // twitter: {
-        //     url: 'https://x.com/nicole',
-        //     icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/X_logo_2023.svg/2268px-X_logo_2023.svg.png'
-        // },
-        email: {
-            url: 'mailto:hey.itsnicolechristine@gmail.com',
-            icon: 'https://cdn-icons-png.flaticon.com/512/732/732200.png'
-        },
-        tiktok: {
-            url: 'https://tiktok.com/@its.nicole.christine',
-            icon: 'https://sf-tb-sg.ibytedtos.com/obj/eden-sg/uhtyvueh7nulogpoguhm/tiktok-icon2.png'
-        },
-        youtube: {
-            url: 'https://youtube.com/@itsnicolechristine',
-            icon: 'https://cdn-icons-png.flaticon.com/512/25/25231.png'
-        },
-        // Alert email for Brand Deals
-        brandDealsEmail: 'hey.itsnicolechristine@gmail.com'
-    },
-    security: {
-        sessionTimeoutMinutes: 360 // 6 hours
-    },
-    meta: {
-        name: 'Nicole Christine',
-        tagline: 'Creator & Model',
-        copyright: 'Nicole Christine',
-        operator: 'ILYTAT LLC'
-    },
-    hoverMessages: [
-        "Have a beautiful day!",
-        "You look great today!",
-        "Stay inspired.",
-        "Welcome home.",
-        "You belong here.",
-        "Keep shining!",
-        "Make it happen."
-    ],
+    socials,
+    security,
+    meta,
+    hoverMessages,
     theme: {
         active: 'default',
         themes: themes
     },
-    ui: ui
+    ui,
+    quickLaunch
 })
---
+++ config/content.ts
@@ -0,0 +1,9 @@
+export const hoverMessages = [
+    "Have a beautiful day!",
+    "You look great today!",
+    "Stay inspired.",
+    "Welcome home.",
+    "You belong here.",
+    "Keep shining!",
+    "Make it happen."
+]
